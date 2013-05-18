e40d11ce454899de0e280c4008d51877e43a826e
Vlad Didenko <business@didenko.com>
2013-05-18 14:19:41 -0700

    First working documented copy.

---
+++ fanout.go
@@ -0,0 +1,78 @@
+// Pumps is a collection of channel manipulators. Contained objectsexpected to communicate via channels, including meta-communication.
+package pumps
+
+import (
+	"reflect"
+)
+
+// `target` struct stores channels which subscribed to a specific fan. With each consumer channel `target` keeps pre-extracted it's `reflect.Type` to speed up message processing.
+type target struct {
+	ch  reflect.Value
+	typ reflect.Type
+}
+
+// `FanOut` stores the in- and out-flow channel information. `FanOut` should not be instantiated directly, but rather via the `MakeFanOut` function. As this object expected to be used to route errors it itself does not emit errors to avoid ambiguity. For example, if a no-channel value fed into the `Outs` intake channel the code will panic while attempting to reflect on its element's type or later when it tries to post into it.
+type FanOut struct {
+
+	// `Post` channel accepts messages to be fanned out to relevant downstream channels provided by users. The `Post` channel buffer size provided as a parameter to `MakeFanOut`. Sending `nil` to the `Post` channel stops the FanOut goroutines, closes all channels and allows the object to be garbage-collected.
+	Post chan interface{}
+
+	// `Outs` channel accepts "subscriber" channels. Messages received on the `Post` channel will be sent to each of channels received earlier on the via `Outs`, where the meggase value is assignable to the ouf-flow channel value. Sending `nil` to the `Outs` channel stops subscription goroutine and closes the `Outs` channel - preventing future subscriptions.
+	Outs  chan interface{}
+	users []target
+}
+
+// The `MakeFanOut` function is the main entry point. The only parameter to it is the buffer size of the intake channel.
+func MakeFanOut(bufsize int) *FanOut {
+
+	fan := &FanOut{
+		make(chan interface{}, bufsize),
+		make(chan interface{}, 1),
+		[]target{},
+	}
+
+	go fan.subscrLoop()
+	go fan.messageLoop()
+
+	return fan
+}
+
+func (fan *FanOut) subscrLoop() {
+
+	for userCh := range fan.Outs {
+
+		if userCh == nil {
+			close(fan.Outs)
+			break
+		}
+
+		elemType := reflect.TypeOf(userCh).Elem()
+
+		fan.users = append(fan.users, target{
+			reflect.ValueOf(userCh),
+			elemType,
+		})
+	}
+}
+
+func (fan *FanOut) messageLoop() {
+
+	for msg := range fan.Post {
+
+		if msg == nil {
+			fan.Outs <- nil
+			close(fan.Post)
+			break
+		}
+
+		msgType := reflect.TypeOf(msg)
+
+		for _, tgt := range fan.users {
+			if msgType.AssignableTo(tgt.typ) {
+				go func(chValue reflect.Value) {
+					chValue.Send(reflect.ValueOf(msg))
+				}(tgt.ch)
+			}
+		}
+	}
+}
---
+++ fanout_test.go
@@ -0,0 +1,133 @@
+package pumps
+
+import (
+	"errors"
+	"os"
+	"testing"
+	"time"
+)
+
+const constInt int = 42
+const constStr string = "ABC"
+
+func TestPlainError(t *testing.T) {
+	var resBits int8 = 0
+
+	bcast := MakeFanOut(2)
+
+	AllErrors := make(chan error)
+	bcast.Outs <- AllErrors
+	var AllErrorsBit int8 = 1
+
+	LinkErrors := make(chan *os.LinkError)
+	bcast.Outs <- LinkErrors
+	var LinkErrorsBit int8 = 2
+
+	errPlain := errors.New(constStr)
+
+	bcast.Post <- errPlain
+
+	select {
+	case err := <-AllErrors:
+		if err.Error() != constStr {
+			t.Errorf("Wrong \"%s\" on generic error channel.", err.Error())
+		} else {
+			resBits |= AllErrorsBit
+		}
+	case err := <-LinkErrors:
+		if err.Error() != constStr {
+			t.Errorf("Wrong \"%s\" on link error channel.", err.Error())
+		} else {
+			resBits |= LinkErrorsBit
+		}
+	case <-time.After(5 * time.Millisecond):
+		t.Error("Did not receive the error on generic error channel.")
+	}
+
+	bcast.Post <- nil
+
+	if resBits != AllErrorsBit {
+		t.Errorf("Incorrect result bitmap: %d", resBits)
+	}
+}
+
+func TestThreeErrors(t *testing.T) {
+	var resBits int8 = 0
+
+	bcast := MakeFanOut(2)
+
+	AllErrors := make(chan error)
+	bcast.Outs <- AllErrors
+	var AllErrorsBit int8 = 1
+
+	LinkErrors := make(chan *os.LinkError)
+	bcast.Outs <- LinkErrors
+	var LinkErrorsBit int8 = 2
+
+	PathErrors := make(chan *os.PathError)
+	bcast.Outs <- PathErrors
+	var PathErrorsBit int8 = 4
+
+	pathError := &os.PathError{
+		"Remove",
+		"/mach_kernel",
+		errors.New("permission denied"),
+	}
+
+	bcast.Post <- pathError
+
+waitForResults:
+	for {
+		select {
+		case err := <-AllErrors:
+			if err.Error() != "Remove /mach_kernel: permission denied" {
+				t.Errorf("Wrong \"%s\" on generic errors channel.", err.Error())
+			} else {
+				resBits |= AllErrorsBit
+			}
+		case err := <-PathErrors:
+			if err.Error() != "Remove /mach_kernel: permission denied" {
+				t.Errorf("Wrong \"%s\" on path errors channel.", err.Error())
+			} else {
+				resBits |= PathErrorsBit
+			}
+		case <-LinkErrors:
+			resBits |= LinkErrorsBit
+		case <-time.After(5 * time.Millisecond):
+			break waitForResults
+		}
+	}
+
+	bcast.Post <- nil
+
+	if resBits != AllErrorsBit|PathErrorsBit {
+		t.Errorf("Incorrect result bitmap: %d", resBits)
+	}
+}
+
+func TestBroadcastPrimitiveAndError(t *testing.T) {
+
+	bcast := MakeFanOut(1)
+
+	chanInt := make(chan int)
+	chanErr := make(chan error)
+
+	bcast.Outs <- chanInt
+	bcast.Outs <- chanErr
+
+	bcast.Post <- constInt
+
+	select {
+	case gotInt := <-chanInt:
+		if gotInt != constInt {
+			t.Error("Got a wrong value on the integer channel")
+		}
+	case <-chanErr:
+		t.Error("Got value on the error channel, was not supposed to.")
+	case <-time.After(5 * time.Millisecond):
+		t.Error("Did not receive expected values on channels")
+	}
+
+	bcast.Post <- nil
+
+}
